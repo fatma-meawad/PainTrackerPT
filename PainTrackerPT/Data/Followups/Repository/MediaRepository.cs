3c491dbc2b20f98599a7b4db1338088edb6fe010
Samuel! :) <29274144+6cap-samuel@users.noreply.github.com>
2019-03-31 03:27:26 +0800

    Resolve conflict

--- PainTrackerPT/Data/Followups/Repository/MediaRepository.cs
+++ PainTrackerPT/Data/Followups/Repository/MediaRepository.cs
@@ -13,20 +13,9 @@ using PainTrackerPT.Models;
 using PainTrackerPT.Models.Followups;
 using PainTrackerPT.Data.Followups.Repository;
 
-
-
 namespace PainTrackerPT.Data.Followups
 {
-<<<<<<< HEAD
 
-    public class MediaRepository : BaseRepository
-    {
-        DbSet<Media> MediaSet;
-
-        public MediaRepository(PainTrackerPTContext db) : base(db)
-        {
-            MediaSet = db.Set<Media>();
-=======
     public class MediaRepository : BaseRepository
     {
         private DbSet<Media> _mediaSet;
@@ -34,36 +23,10 @@ namespace PainTrackerPT.Data.Followups
         public MediaRepository(PainTrackerPTContext db) : base(db)
         {
             _mediaSet = db.Set<Media>();
->>>>>>> 6cffa594ce4f0cbe2ec4cfbca324ebf850db754f
         }
 
         public void Create(Media media)
         {
-<<<<<<< HEAD
-            this.MediaSet.Add(media);
-            db.SaveChanges();
-        }
-
-        public void Remove(Guid id)
-        {
-            Media media = MediaSet.Find(id);
-            if (media != null)
-            {
-                MediaSet.Remove(media);
-                this.Save();
-            }
-        }
-
-        public async Task<IEnumerable<Media>> SelectAll()
-        {
-            return await MediaSet.ToArrayAsync();
-
-        }
-
-        public async Task<Media> Select(Guid id)
-        {
-            return MediaSet.Find(id);
-=======
             this._mediaSet.Add(media);
             db.SaveChanges();
         }
@@ -87,18 +50,13 @@ namespace PainTrackerPT.Data.Followups
                 _mediaSet.Remove(media);
                 this.Save();
             }
->>>>>>> 6cffa594ce4f0cbe2ec4cfbca324ebf850db754f
         }
 
         public void Update(Media media)
         {
-<<<<<<< HEAD
-            Media ExisitingMedia = MediaSet.Find(media.id);
-            MediaSet.Update(media);
-=======
+
             Media existingMedia = _mediaSet.Find(media.id);
             _mediaSet.Update(media);
->>>>>>> 6cffa594ce4f0cbe2ec4cfbca324ebf850db754f
             this.Save();
         }
     }
