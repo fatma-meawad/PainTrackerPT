5c9f67b711c2312745775977930423e2ae6d48f6
Richmond <1700247@sit.singaporetech.edu.sg>
2019-03-30 22:40:40 +0800

    Baseline Codes

---
+++ PainTrackerPT/Common/PainDiary/ILog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PainTrackerPT.Common.PainDiary
+{
+    interface ILog
+    {
+        void getMood();
+    }
+}
