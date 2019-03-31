3be2a973dbfc2a3c1f7f146103202f42b0f6cd78
adventl <1701070@sit.singaporetech.edu.sg>
2019-04-01 03:20:51 +0800

    Updated Models and Table Configurations

--- PainTrackerPT/Models/Followups/FollowUp.cs
+++ PainTrackerPT/Models/Followups/FollowUp.cs
@@ -10,11 +10,11 @@ namespace PainTrackerPT.Models.Followups
     {
         public Guid PatientId { get; set; }
         public Guid DoctorId { get; set; }
-        [Required]
-        [Range(1, 5, ErrorMessage = "Please enter a valid state (1-5)")]
         public int State { get; set; }
         [Required]
         public string Description { get; set; }
-        public DateTime DateGenerated { get; set; } 
+        public DateTime DateGenerated { get; set; }
+
+        public virtual ICollection<Question> Questions { get; set; }
     }
 }
