bc26b8c2f84d6fb46292e7fd41fe245edfca1c98
Samuel! :) <29274144+6cap-samuel@users.noreply.github.com>
2019-04-02 03:40:14 +0800

    Revert "Merge branch 'followups' of https://github.com/SingaporeTechEDU/PainTrackerPT into followups"
    This reverts commit c33b3d8de827e7d92304bfff2be4634d5016eb57, reversing
    changes made to c52bc6e768210f5442e99c9ca7693c75a232733d.

--- PainTrackerPT/Models/Followups/Recommendation.cs
+++ PainTrackerPT/Models/Followups/Recommendation.cs
@@ -8,9 +8,9 @@ namespace PainTrackerPT.Models.Followups
 {
     public class Recommendation : BaseEntity
     {
-        public Int64 AnswerId { get; set; }
+        public Int64 AnswerId { get => AnswerId; set => AnswerId = value; }
         [Required]
-        public String Description { get; set; }
-        public DateTime DateGenerated { get; set; }
+        public String Description { get => Description; set => Description = value; }
+        public DateTime DateGenerated { get => DateGenerated; set => DateGenerated = value; }
     }
 }
